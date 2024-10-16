cf6d98bece8fff7905a42211428016c9eabac9a8
nobel <nobel2073@gmail.com>
2024-10-16 21:41:46 +0545

    use logrus instead of default logger

--- main.go
+++ main.go
@@ -2,37 +2,47 @@ package main
 
 import (
 	"io"
-	"log"
 	"net/http"
+	"os"
+
+	log "github.com/sirupsen/logrus"
 )
 
+func InitLogger() {
+	log.SetOutput(os.Stdout)
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+}
+
 func main() {
+	InitLogger()
 	listenAddr := ":8080"
 
-	log.Printf("INFO: Starting proxy server on %s...", listenAddr)
+	log.Infof("Starting proxy server on %s...", listenAddr)
 	if err := http.ListenAndServe(listenAddr, http.HandlerFunc(handleRequest)); err != nil {
-		log.Fatalf("ERROR: Failed to start server: %v", err)
+		log.Errorf("Failed to start server: %v", err)
 	}
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	log.Printf("INFO: Received request %s %s", r.Method, r.URL.String())
+	log.Infof("Received request %s %s", r.Method, r.URL.String())
 
 	resp, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
 		http.Error(w, "Error forwarding request: "+err.Error(), http.StatusServiceUnavailable)
-		log.Printf("ERROR: Failed to forward request: %v", err)
+		log.Errorf("Failed to forward request: %v", err)
 		return
 	}
 	defer resp.Body.Close()
 
-	log.Printf("INFO: Forwarded request to %s with status %d", r.URL.String(), resp.StatusCode)
+	log.Infof("Forwarded request to %s with status %d", r.URL.String(), resp.StatusCode)
 
 	copyHeader(w.Header(), resp.Header)
 	w.WriteHeader(resp.StatusCode)
 
 	if _, err := io.Copy(w, resp.Body); err != nil {
-		log.Printf("ERROR: Failed to copy response body: %v", err)
+		log.Errorf("Failed to copy response body: %v", err)
 	}
 }
 
